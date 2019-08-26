cc4b689f9ab5b83446a2bc74496fa4fa5a47ccd4
moxdev <moxdev1@gmail.com>
2019-08-26 15:27:52 -0400

    curves header mobile done

--- inc/components/front-page/fp-featured-img.php
+++ inc/components/front-page/fp-featured-img.php
@@ -75,7 +75,7 @@ if ( ! function_exists( 'expedite_delivery_system_front_page_feature' ) ) :
 								$link_title  = $button['title'];
 								$link_target = $button['target'] ? $button['target'] : '_self';
 								?>
-								<a class="btn" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?>&nbsp;<span>&raquo;</span></a>
+								<a class="btn" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
 								<?php
 							endif;
 							?>
